1b21e56dfb26f500899d33ed84e7cbfd7b459156
Rockruff <36600169+Rockruff@users.noreply.github.com>
2024-09-26 23:09:18 +0800

    chore: fix git hooks for Linux

--- setup-git-hooks.sh
+++ setup-git-hooks.sh
@@ -8,23 +8,40 @@ source "$REPO/.githooks/functions.sh"
     git config commit.cleanup strip
     git config core.hooksPath .githooks
 )
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
     success 'Git hooks configured for local repository.'
 else
     error 'Failed to configure git hooks for local repository.'
     exit 1
 fi
 
-# Install formatters
+# Install Python code formatter
 (
     set -e -x
-    py -m pip install black >/dev/null 2>&1
-    npm install --global prettier >/dev/null 2>&1
+    if command -v apt >/dev/null; then
+        sudo apt install black -y >/dev/null
+    else
+        py -m pip install black >/dev/null
+    fi
 )
-if [ $? -eq 0 ]; then
-    success 'Code formatters installed successfully.'
+if [[ $? -eq 0 ]]; then
+    success 'Python code formatter installed successfully.'
 else
-    error 'Failed to install code formatters for git hook.'
-    error 'Please ensure that both Python 3 and Node.js are installed on your system.'
+    error 'Failed to install Python code formatter for git hook.'
+    error 'Please ensure that Python 3 is installed on your system.'
+    exit 1
+fi
+
+# Install frontend code formatter
+(
+    set -e -x
+    cd "$REPO/frontend"
+    npm install --save-dev prettier prettier-plugin-classnames
+)
+if [[ $? -eq 0 ]]; then
+    success 'Frontend code formatter installed successfully.'
+else
+    error 'Failed to install frontend code formatter for git hook.'
+    error 'Please ensure Node.js and Npm is installed on your system.'
     exit 1
 fi
