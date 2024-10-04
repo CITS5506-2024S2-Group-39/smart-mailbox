05de66b3fae382144b7aab9f13591b9958922c7f
Rockruff <36600169+Rockruff@users.noreply.github.com>
2024-10-05 00:44:30 +0800

    fix(backend): fix init script and exclude changes to apikey.txt

--- backend/env.sh
+++ backend/env.sh
@@ -1,9 +1,12 @@
 #!/bin/env bash
-# Sourcing this file will install relevant packages
-set -e
 REPO=$(git rev-parse --show-toplevel)
 cd "$REPO/backend"
-rm -r venv
+
+# Sourcing this file will install relevant packages
+rm -rf venv
 py -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+
+# Exclude changes to apikey.txt to avoid accidental commits of sensitive information
+git update-index --assume-unchanged "$REPO/backend/gpt/apikey.txt"
